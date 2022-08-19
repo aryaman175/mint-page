800fa3932ea5017fa5ed295f0d46652f7ff39760
Aryaman <aryamankumar02@gmail.com>
2022-08-19 11:54:14 +0530

    created UI to buy NFTs

---
+++ components/purchase/purchase.js
@@ -0,0 +1,16 @@
+import React from "react";
+import classes from ".././menu/menu.module.css"
+
+export default function Purchase() {
+  return (
+    <div className={`${classes.container} ${classes.purchase}`}>
+      <div className={classes.text}>
+        <h1>JOIN KICKBACK</h1>
+        <h4>BUY AN NFT AND JOIN THE FAN COMMMUNITY FOR EXCLUSIVE PERKS</h4>
+        <button>BUY NOW</button>
+      </div>
+      <img src="photos/NFT.png" />
+    </div>
+  
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
 import React from "react";
 import Menu from "../components/menu/menu";
+import Purchase from "../components/purchase/purchase";
 
 export default function MintPage()
 {
   return (
-    <Menu></Menu>
+    <>
+    <Menu />
+    <Purchase />
+    </>
   );
 }
